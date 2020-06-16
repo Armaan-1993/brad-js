32d90897eafab53acd9c1832905e1f3427a96f93
Armaan-1993 <armaanmathew88@gmail.com>
2020-06-16 10:28:35 +0530

    js tutuorial section 3

---
+++ 03_dom_manupulation_events/023_examining_document_object.js
@@ -0,0 +1,29 @@
+let val;
+
+val = document;
+val = document.all;
+// val = document.all[6];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.dispatchEvent;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+val = document.forms[0];
+val = document.forms[0].action;
+val = document.forms[0].id;
+
+val = document.links;
+// val = document.links[0];
+// val = document.links[0].href;
+// val = document.links[0].id;
+
+// // val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[0].getAttribute("src");
+
+console.log(val);
