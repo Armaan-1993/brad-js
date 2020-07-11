46ea7246878f85b83a65868681bdcf610acc7d68
Armaan-1993 <armaanmathew88@gmail.com>
2020-07-11 10:06:17 +0530

    Section 7 tutorial js

---
+++ 07_asynjs_and_ajax/067_fetch_api.js
@@ -0,0 +1,54 @@
+document.getElementById("button1").addEventListener("click", getText);
+
+document.getElementById("button2").addEventListener("click", getJson);
+
+document.getElementById("button3").addEventListener("click", getExternal);
+
+//Fetching text from a text file
+function getText() {
+    fetch("067_fetch_api.txt")
+        .then(function (res) {
+            return res.text();
+        })
+        .then(function (data) {
+            // console.log(data);
+            document.getElementById("output").innerHTML = data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+//function to fetch json from json file
+
+function getJson() {
+    fetch("067_fetch_api.json")
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            let output = "";
+            data.forEach(function (post) {
+                output += `<li>${post.title}</li>`;
+            });
+            document.getElementById("output").innerHTML = output;
+        });
+}
+
+//function to fetch json from external API
+
+function getExternal() {
+    fetch(`https://api.github.com/users`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            let output = "";
+            data.forEach(function (user) {
+                output += `<li>${user.login}</li>`;
+            });
+            document.getElementById("output").innerHTML = output;
+        });
+}
