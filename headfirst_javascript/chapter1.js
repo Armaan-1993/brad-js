210cf7cfb02e2788321624820c710f57186e35bb
Armaan-1993 <armaanmathew88@gmail.com>
2020-07-31 01:56:28 +0530

    chapters 1 and 2

---
+++ headfirst_javascript/chapter1.js
@@ -0,0 +1,49 @@
+// setTimeout(wakeUpUser, 5000);
+// function wakeUpUser() {
+//     alert("The user needs to go wake up and do something generic"); //used to alert the user woth a message
+// }
+
+//page 8
+// let price = 28.99;
+// let discount = 10;
+// let total = price - price * (discount / 100);
+// if (total > 25) {
+//     freeshipping();
+// }
+
+// let count = 10;
+// while (count > 0) {
+//     juggle();
+//     count = count - 1; //count--
+// }
+
+// let dog = { name: "Rover", weight: 35 };
+// if (dog.weight > 30) {
+//     alert("WOOF WOOF");
+// } else {
+//     alert("woof woof");
+// }
+
+// let circleRadius = 20;
+// let circleArea = Math.PI * (circleRadius * circleRadius);
+
+// let age = 25;
+// var name = "Owen";
+// if (age > 14) {
+//     alert("this page is not for kids , but for adults");
+// } else {
+//     alert("this page is for kids");
+// }
+
+// while(age > 0) {
+//     document.
+//     alert("*");
+//     alert("The child is born")
+// }
+
+// let scooop = 5;
+// while (scooop > 0) {
+//     document.write("Another Scoop!<br>");
+// scooop = scooop -1;
+// }
+// document.write("Life without ice cream isn't the same");
