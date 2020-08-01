d25b196007a1ea6c68272ed8fa008163dcb6894c
Armaan-1993 <armaanmathew88@gmail.com>
2020-08-01 14:40:01 +0530

    chapter 4 in progress

--- headfirst_javascript/chapter4.js
+++ headfirst_javascript/chapter4.js
@@ -1,1 +1,70 @@
 //arrays
+// let array = [11, 34, 56, 78];
+// array[3] = 89;
+// console.log(array);
+
+// let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+// let hasBubbleGum = [false, false, false, true];
+// for (let i = 0; i < hasBubbleGum.length; i++) {
+//     for (let j = 0; j < products.length; j++) {
+//         if (hasBubbleGum[i]) {
+//             console.log(`${products[j]} conatins ${products[j]}`);
+//         }
+//     }
+// }
+
+// var hasBubbleGum = [false, false, false, true];
+// var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+// var i = 0;
+// while (i < hasBubbleGum.length) {
+//     if (hasBubbleGum[i]) {
+//         console.log(products[i] + " contains bubble gum");
+//     }
+//     i++;
+// }
+
+let scores = [
+    60,
+    50,
+    60,
+    58,
+    54,
+    54,
+    58,
+    50,
+    52,
+    54,
+    48,
+    69,
+    34,
+    55,
+    51,
+    52,
+    44,
+    51,
+    69,
+    64,
+    66,
+    55,
+    52,
+    61,
+    46,
+    31,
+    57,
+    52,
+    44,
+    18,
+    41,
+    53,
+    55,
+    61,
+    51,
+    44,
+];
+// let i = 0;
+// while (i < scores.length) {
+for (let i = 0; i < scores.length; i++) {
+    let greatest;
+    console.log(`Bubble Solution #${i} score = ${scores[i]}`);
+}
+console.log(`The total number of bubble scores = ${scores.length}`);
