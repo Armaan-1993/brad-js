1fae677791ec4ffed7d6e28ef0258110d1ceee77
Armaan-1993 <armaanmathew88@gmail.com>
2020-08-07 02:40:15 +0530

    chapter7

--- headfirst_javascript/chapter7.js
+++ headfirst_javascript/chapter7.js
@@ -4,3 +4,36 @@
 // getName("Armaan");
 
 // console.log("This is a new function");
+
+//checking undefined for varibales
+
+// let name;
+// function checkForDefinition() {}
+// if (name === undefined) {
+//     console.log("The variable is undefined");
+// } else {
+//     console.log("Its not undefined");
+// }
+// checkForDefinition();
+
+//checking undefined for objects
+
+// let customer = {
+//     name: "Armaan",
+//     age: 26,
+// };
+// function checkObject(object) {
+//     if (object.place === undefined) {
+//         console.log("The property is undefined");
+//     } else {
+//         console.log("The property is defined");
+//     }
+// }
+// checkObject(customer);
+
+let name = "Armaan";
+console.log(typeof name);
+function test() {
+    return name;
+}
+console.log(typeof test);
