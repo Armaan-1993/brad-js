97cd664646eb3ed78f0c79751bbe4248b173ad31
armaan <armaanmathew88@gmail.com>
2020-08-14 01:58:14 +0530

    chapter9

--- headfirst_javascript/chapter9.js
+++ headfirst_javascript/chapter9.js
@@ -1,3 +1,15 @@
-function pageLoadEvent() {
-  alert("I am alive since the DOM has finished being loaded by the browser!");
+// function pageLoadEvent() {
+//   alert("I am alive since the DOM has finished being loaded by the browser!");
+// }
+// window.onload = pageLoadEvent;
+
+function loadImage() {
+  let image = document.getElementById("zeroBlurred");
+  image.addEventListener("click", showUnblurred);
+}
+window.onload = loadImage;
+
+function showUnblurred() {
+  let image = document.getElementById("zeroBlurred");
+  image.src = "img/zero.jpg";
 }
