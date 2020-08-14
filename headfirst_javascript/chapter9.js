b028bdb986025e924d85bec9fc818e36f7ddcc19
armaan <armaanmathew88@gmail.com>
2020-08-15 01:30:34 +0530

    chapter9

--- headfirst_javascript/chapter9.js
+++ headfirst_javascript/chapter9.js
@@ -3,13 +3,111 @@
 // }
 // window.onload = pageLoadEvent;
 
-function loadImage() {
-  let image = document.getElementById("zeroBlurred");
-  image.addEventListener("click", showUnblurred);
-}
-window.onload = loadImage;
+// function loadImage() {
+//   let image = document.getElementById("zeroBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// function loadImage() {
+//   let image = document.getElementById("oneBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// function loadImage() {
+//   let image = document.getElementById("twoBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// function loadImage() {
+//   let image = document.getElementById("threeBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// function loadImage() {
+//   let image = document.getElementById("fourBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// function loadImage() {
+//   let image = document.getElementById("fiveBlurred");
+//   image.addEventListener("click", showUnblurred);
+// }
+// window.onload = loadImage;
 
-function showUnblurred() {
-  let image = document.getElementById("zeroBlurred");
-  image.src = "img/zero.jpg";
-}
+// function showUnblurred() {
+//   let image = document.getElementById("zeroBlurred");
+//   image.src = "img/zero.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("oneBlurred");
+//   image.src = "img/one.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("twoBlurred");
+//   image.src = "img/two.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("threeBlurred");
+//   image.src = "img/three.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("fourBlurred");
+//   image.src = "img/four.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("fiveBlurred");
+//   image.src = "img/five.jpg";
+// }
+
+//alternate method
+// function pageLoadEvent() {
+//   let images = document.getElementsByTagName("img");
+//   // console.log(images);
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].addEventListener("click", showUnblurred);
+//   }
+// }
+// window.onload = pageLoadEvent;
+// function showUnblurred() {
+//   let image = document.getElementById("zeroBlurred");
+//   image.src = "img/zero.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("oneBlurred");
+//   image.src = "img/one.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("twoBlurred");
+//   image.src = "img/two.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("threeBlurred");
+//   image.src = "img/three.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("fourBlurred");
+//   image.src = "img/four.jpg";
+// }
+// function showUnblurred() {
+//   let image = document.getElementById("fiveBlurred");
+//   image.src = "img/five.jpg";
+// }
+
+// function eventHandler(eventObject) {
+//   let element = eventObject.target;
+//   console.log(element);
+// }
+
+//image load game
+
+// function pageLoadEvent() {
+//   let images = document.getElementsByTagName("img");
+//   // console.log(images);
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].addEventListener("click", showUnblurred);
+//   }
+// }
+// window.onload = pageLoadEvent;
+
+// function showUnblurred(eventObject) {
+//   let image = eventObject.target;
+//   let idName = image.id;
+//   idName = idName + ".jpg";
+//   image.src = idName;
+// }
+// //
