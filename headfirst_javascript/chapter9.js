545929b7855feb6df4d9526870d723deee926deb
armaan <armaanmathew88@gmail.com>
2020-08-15 20:01:52 +0530

    chapter9

--- headfirst_javascript/chapter9.js
+++ headfirst_javascript/chapter9.js
@@ -111,3 +111,58 @@
 //   image.src = idName;
 // }
 // //
+
+// function init() {
+//   let map = document.getElementById("map");
+//   map.addEventListener("mousemove", showCoords);
+// }
+// window.onload = init;
+
+// function showCoords(eventObject) {
+//   let map = document.getElementById("coords");
+//   let x = eventObject.pageX;
+//   let y = eventObject.pageY;
+//   map.innerHTML = "The Map Coordinates are : " + x + " , " + y;
+// }
+
+//setTimeout
+// function testTimeout() {
+//   alert("What on earth are you doing looking at the screen!");
+// }
+// setTimeout(testTimeout, 5000);
+
+//setInterval - ask doubt
+// let test = true;
+// function testsetInterval() {
+//   if (test) {
+//     console.log("tick");
+//     test = false;
+//   } else {
+//     console.log("tock");
+//     test = true;
+//   }
+// }
+// setInterval(testsetInterval, 1000);
+
+//image load game finalised:
+function pageLoadEvent() {
+  let images = document.getElementsByTagName("img");
+  // console.log(images);
+  for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", showUnblurred);
+  }
+}
+window.onload = pageLoadEvent;
+
+function showUnblurred(eventObject) {
+  let image = eventObject.target;
+  let idName = image.id;
+  idName = idName + ".jpg";
+  image.src = idName;
+  setTimeout(reblur, 2000, image);
+}
+function reblur(image) {
+  let idName = image.id;
+  idName = idName + "blur.jpg";
+  image.src = idName;
+}
