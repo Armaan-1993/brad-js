ef47ac634637b54a00a3d6b15ba7b303b4e5f09f
armaan <armaanmathew88@gmail.com>
2020-08-18 04:12:44 +0530

    chapter11

---
+++ headfirst_javascript/chapter11.js
@@ -0,0 +1,34 @@
+// function handle() {
+//   alert("Everythig is good");
+// }
+// window.onload = handle;
+
+// window.onload = function () {
+//   alert("Everythig is good");
+// };
+
+// setTimeout(function () {
+//   alert("The cookies are done baking! Time to get them out of the oven");
+// }, 6000);
+
+// function vaccine(dosage) {
+//   if (dosage > 0) {
+//     inject(dosage);
+//   }
+// }
+// administer(patient, vaccine, time);
+
+//using varible to call and returm
+
+// let vaccine = function (dosage) {
+//   if (dosage > 0) {
+//     inject(dosage);
+//   }
+// };
+// administer(patient, vaccine, time);
+
+name("Armaan");
+let name = function (myName) {
+  console.log(`My name is ${myName}`);
+};
+// name("Armaan");
