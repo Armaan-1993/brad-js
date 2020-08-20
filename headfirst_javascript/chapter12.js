c6f3c21529db93bf67c0a7ff7db993623c24fbb6
armaan <armaanmathew88@gmail.com>
2020-08-21 00:23:50 +0530

    chpater 12 and 13

--- headfirst_javascript/chapter12.js
+++ headfirst_javascript/chapter12.js
@@ -83,48 +83,179 @@
 // let nescafe = new Coffee("Roasted Nescafe", 7);
 
 //cars
-function Cars(
-  make,
-  model,
-  year,
-  color,
-  passenger,
-  convertable,
-  mileage,
-  started
-) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.color = color;
-  this.passenger = passenger;
-  this.convertale = convertable;
-  this.mileage = mileage;
-  this.started = started;
-  this.start = function () {
-    if (this.started === false) {
-      this.started = true;
-      console.log("The car has started now");
-    }
-  };
+// function Cars(
+//   make,
+//   model,
+//   year,
+//   color,
+//   passenger,
+//   convertable,
+//   mileage,
+//   started
+// ) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+//   this.color = color;
+//   this.passenger = passenger;
+//   this.convertale = convertable;
+//   this.mileage = mileage;
+//   this.started = started;
+//   this.start = function () {
+//     if (this.started === false) {
+//       this.started = true;
+//       console.log("The car has started now");
+//     }
+//   };
+// }
+// let chevy = new Cars("Chevy", "Bel Air", 1957, "red", 2, false, 1021, false);
+// let taxi = new Cars(
+//   "Webville Motors",
+//   "Taxi",
+//   1955,
+//   "yellow",
+//   4,
+//   false,
+//   281341
+// );
+// let arrayOfCars = [chevy, taxi];
+// for (let i = 0; i < arrayOfCars.length; i++) {
+//   arrayOfCars[i].start();
+//   console.log(`${arrayOfCars[i].make} has started`);
+//   // if (arrayOfCars[i].mileage < 2000) {
+//   //   console.log("Mileage is less");
+//   // }
+// }
+// chevy.start();
+// taxi.start();
+
+// //rewriting the car constructor
+// function Cars(carParameters) {
+//   this.make = carParameters.make;
+//   this.model = carParameters.model;
+//   this.year = carParameters.year;
+//   this.color = carParameters.color;
+//   this.passenger = carParameters.passenger;
+//   this.convertale = carParameters.convertable;
+//   this.mileage = carParameters.mileage;
+//   this.started = carParameters.started;
+//   this.start = function () {
+//     if (this.started === false) {
+//       this.started = true;
+//       console.log("The car has started now");
+//     }
+//   };
+// }
+// let taxiObj = {
+//   make: "Webville Motors",
+//   model: "Taxi",
+//   year: 1955,
+//   color: "yellow",
+//   passengers: 4,
+//   convertible: false,
+//   mileage: 281341,
+//   started: false,
+// };
+// let chevyObj = {
+//   make: "Chevy",
+//   model: "Bel Air",
+//   year: 1957,
+//   color: "red",
+//   passengers: 4,
+//   convertible: false,
+//   mileage: 10787,
+//   started: false,
+// };
+// let taxi = new Cars(taxiObj);
+// let chevy = new Cars(chevyObj);
+// let arrayOfCars = [chevy, taxi];
+// for (let i = 0; i < arrayOfCars.length; i++) {
+//   arrayOfCars[i].start();
+//   console.log(`${arrayOfCars[i].make} has started`);
+//   // if (arrayOfCars[i].mileage < 2000) {
+//   //   console.log("Mileage is less");
+//   // }
+// }
+// chevy.start();
+// taxi.start();
+// for (let i = 0; i < arrayOfCars.length; i++) {
+//   if (arrayOfCars[i] instanceof Cars) {
+//     console.log(`${arrayOfCars[i]} is an instance of the Car Constructor`);
+//   }
+// }
+
+//dog catcher function
+// function dogCatcher(obj) {
+//   if (obj instanceof Dog) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function Cat(name, breed, weight) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.weight = weight;
+// }
+// let meow = new Cat("Meow", "Siamese", 10);
+// let whiskers = new Cat("Whiskers", "Mixed", 12);
+
+// function Dog(name, breed, weight) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.weight = weight;
+//   this.bark = function () {
+//     if (this.weight > 25) {
+//       console.log(this.name + " says Woof!");
+//     } else {
+//       console.log(this.name + " says Yip!");
+//     }
+//   };
+// }
+// let fidoObj = { name: "Fido", breed: "Mixed", weight: 38 };
+// let fido = new Dog(fidoObj);
+// let fluffy = new Dog("Fluffy", "Poodle", 30);
+// let spot = new Dog("Spot", "Chihuahua", 10);
+// let dogs = [meow, whiskers, fido, fluffy, spot];
+// for (let i = 0; i < dogs.length; i++) {
+//   if (dogCatcher(dogs[i])) {
+//     console.log(`${dogs[i].name} is a dog`);
+//   }
+// }
+
+// let now = new Date();
+// let mydate = now.toString();
+// let mydate = now.getFullYear();
+// mydate = now.getMilliseconds();
+// mydate = now.getUTCDate();
+// mydate = now.getDay();
+// console.log(mydate);
+
+// let mydateObj = new Date("1 October 1993 23:09 pm");
+// console.log(mydateObj);
+// delete Object.name;
+// Object.age = 15;
+
+// let newArray = new Array(3);
+// newArray[0] = 5;
+// newArray[1] = 10;
+// // newArray[2] = function () {
+// //   console.log("Hello World");
+// // };
+// newArray[2] = 15;
+// let result = newArray.every(function (x) {
+//   return x % 5 === 0;
+// });
+// console.log(newArray.reverse());
+// console.log(result);
+
+let myarray = new Array("a", "b", "x");
+// console.log(myarray);
+
+function number(x) {
+  console.log(x);
 }
-let chevy = new Cars("Chevy", "Bel Air", 1957, "red", 2, false, 1021, false);
-let taxi = new Cars(
-  "Webville Motors",
-  "Taxi",
-  1955,
-  "yellow",
-  4,
-  false,
-  281341
-);
-let arrayOfCars = [chevy, taxi];
-for (let i = 0; i < arrayOfCars.length; i++) {
-  arrayOfCars[i].start();
-  console.log(`${arrayOfCars[i].make} has started`);
-  // if (arrayOfCars[i].mileage < 2000) {
-  //   console.log("Mileage is less");
-  // }
+for (let i = 0; i < myarray.length; i++) {
+  number(myarray[i]);
 }
-chevy.start();
-taxi.start();
